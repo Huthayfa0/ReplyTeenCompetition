a34dccbc2db40eba79f0cc6b05d33c3a5b971e88
Huthayfa0 <huthifa0@gmail.com>
2021-03-11 15:49:16 +0200

    Second

--- src/Competition2021/A.kt
+++ src/Competition2021/A.kt
@@ -1,5 +1,10 @@
 package Competition2021
 
-fun main(){
+import java.io.File
+import java.io.InputStream
 
+fun main() {
+    val inputStream: InputStream = File("src/Competition2021/AInput/a.in").inputStream()
+    val line=inputStream.bufferedReader().readLine()!!.split(' ')
+    println(line)
 }
